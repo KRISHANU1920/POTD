e4d215e0c0bdbd7b669cf830fedb560510e8ef37
KRISHANU1920 <krishanu1920@gmail.com>
2022-01-05 16:31:38 +0530

    day05 commit

---
+++ day05_gcd.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if(b == 0)
+      return a;
+    else
+       return gcd(b, a%b); 
+}
+
+int main() 
+{ 
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+	cin >> T;
+	while(T--)
+	{
+	    int N;
+	    cin >> N;
+	    int *A = new int [N];
+	    for(int i=0; i<N; i++)
+	        cin >> A[i];
+	    int ans = A[0];
+	    for(int i=1; i<N; i++)
+	        ans = gcd(ans, A[i]);
+	    cout << ans << "\n";
+        delete [] A;
+	}
+	return 0;
+}
+
+/*
+
+Time Complexity: O(T.N.log(max(A[])))
+Space Complexity: O(1)
+
+*/

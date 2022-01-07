fd0e22f98da5a4fc86c1a782afdad9feccf76a2d
KRISHANU1920 <krishanu1920@gmail.com>
2022-01-07 16:24:40 +0530

    optimized

--- day06_PeakElmt.cpp
+++ day06_PeakElmt.cpp
@@ -22,42 +22,23 @@ Space Complextiy: O(1)
 
 // Approach 2: Binary Search
     
-int findPeakElement(vector<int>& nums) {
-    int n = nums.size();
-    int low = 0, high = n - 1;
-    if(n==1)
-        return 0;
-    while(low <= high)
+int findPeakElement(vector<int>& nums) 
+{
+    int start = 0, end = nums.size()-1;
+    while(start < end)
     {
-        int mid = low + (high - low)/2;
-        if(mid > 0 && mid < n-1)
-        {
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
-                return mid;
-            else if(nums[mid-1] > nums[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        else if(mid == 0) {
-            if(nums[mid] > nums[mid+1])
-                return 0;
-            else
-                return 1;
-        }
-        else if(mid == n-1) {
-            if(nums[mid] > nums[mid-1])
-                return n-1;
-            else
-                return n-2;
-        }
+        int mid = start + (end - start)/2;
+        if(nums[mid] < nums[mid+1])
+            start = mid + 1;
+        else 
+            end = mid;
     }
-    return -1;
+    return start;
 }
 
 /*
 
-Time Complextiy: O(logN)
+Time Complextiy: O(log2(N))
 Space Complexity: O(1)
 
 */

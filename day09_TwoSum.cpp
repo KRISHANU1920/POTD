65368b015e3e1401ca859f3ad0f575e5fbb65b9d
KRISHANU1920 <krishanu1920@gmail.com>
2022-01-09 21:47:23 +0530

    day09 commit TwoSum

---
+++ day09_TwoSum.cpp
@@ -0,0 +1,31 @@
+// Question Link: 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans(2,0);
+        int n = numbers.size();
+        int low = 0, high = n-1;
+        while(low < high)
+        {
+            if(numbers[low] + numbers[high] == target)
+            {
+                ans[0] = low + 1;
+                ans[1] = high + 1;
+                return ans;
+            }
+            else if(numbers[low] + numbers[high] < target)
+                low++;
+            else
+                high--;
+        }
+        return ans;
+    }
+};
+
+/*
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+
+*/

55b2458b52e0019c17ff22e82f0c3028557079eb
KRISHANU1920 <krishanu1920@gmail.com>
2022-03-14 12:57:31 +0530

    next Greater Element 2

---
+++ day64.2_nextGreat2.cpp
@@ -0,0 +1,29 @@
+// Question Link : https://leetcode.com/problems/next-greater-element-ii/
+
+vector<int> nextGreaterElements(vector<int>& nums) 
+{
+    stack<int> st;
+    int n = nums.size();
+    vector<int> ans(n, -1);
+    
+    for(int i=2*n-1; i>=0; i--)
+    {
+        int curr = nums[i%n];
+        
+        while(!st.empty() && curr >= st.top()) {
+            st.pop();
+        }
+        
+        ans[i%n] = st.empty() ? -1 : st.top();
+        st.push(curr);
+    }
+    
+    return ans;
+}
+
+/*
+
+Time Complexity: O()
+Space Complexity: O()
+
+*/
